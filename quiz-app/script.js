354b644cfd852ad50353b30cab1fda9d1f36e52c
Your Name <youremail@example.com>
2023-12-25 06:21:42 +0500

    learning

---
+++ quiz-app/script.js
@@ -0,0 +1,33 @@
+const quizData = [
+    {
+        question: "How old is Florin?",
+        a: "10",
+        b: "17",
+        c: "26",
+        d: "110",
+        correct: "c"
+    }, {
+        question: "What is the most used programming language?",
+        a: "java",
+        b: "C",
+        c: "python",
+        d: "javascript",
+        correct: "d"
+    }, {
+        question: "Who is the President of US?",
+        a: "Florin Pop",
+        b: "donald trump",
+        c: "john Mccain",
+        d: "Joe Biden",
+        correct: "d"
+    }, {
+        question: "What does HTML stands for?",
+        a: "Hypertext Markup Language",
+        b: "Cascading Style Sheet",
+        c: "JavaScript Object Notation",
+        d: "Application Programming Interface",
+        correct: "a"
+    }, {
+        question: "What year was "
+    }
+]
